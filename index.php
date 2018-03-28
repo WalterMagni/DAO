65c79c6c97321890dd19b45bf3483ead4cce0035
Walter Magni <waltermagni@terra.com.br>
2018-03-28 18:50:33 -0300

    classe usuario criada

--- index.php
+++ index.php
@@ -2,10 +2,19 @@
 
 	require_once("config.php");
 
+	$walter = new usuario();
+	$walter->loadbyId(1);
+
+	echo $walter;
+
+	/**
+
 	$sql = new Sql();
 
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 	echo json_encode($usuarios);
+
+	**/
 
  ?>
